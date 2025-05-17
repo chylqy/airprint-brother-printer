9152f66e862ff94c3633b3d3662a5bb0c59d13ea
Chris Chen <chylqy@hotmail.com>
2025-05-17 22:58:26 +0800

    change the way of create new user and group

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,22 @@ set -x
 if [ -z "$CUPSADMIN" ]; then
     CUPSADMIN="admin"
 fi
-
 # Is CUPSPASSWORD set? If not, set to $CUPSADMIN
 if [ -z "$CUPSPASSWORD" ]; then
     CUPSPASSWORD=$CUPSADMIN
 fi
-
+# 1. 创建系统用户 admin，不创建 home 目录
 if [ $(grep -ci $CUPSADMIN /etc/shadow) -eq 0 ]; then
-    adduser -S -G lpadmin --no-create-home $CUPSADMIN 
+    adduser --system --no-create-home $CUPSADMIN 
+fi
+# 2. 创建组lpadmin
+if [ $(grep -ci lpadmin /etc/gshadow) -eq 0 ]; then
+    addgroup --system lpadmin
 fi
 echo $CUPSADMIN:$CUPSPASSWORD | chpasswd
 
-# 启动 Avahi 服务
-avahi-daemon --daemonize --no-chroot
+# 启动 Avahi 服务,宿主系统已经有avahi服务，使用host模式的容器可以使用宿主的服务
+#avahi-daemon --daemonize --no-chroot
 
 # 启动 CUPS 服务 (在前台运行，以便 Docker 容器保持活动)
 exec cupsd -f
